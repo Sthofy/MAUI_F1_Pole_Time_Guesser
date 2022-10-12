07233b0fffaa0741edf7b3715cfc5797f4805802
Sthofy <suhajdakristof@gmail.com>
2022-10-12 12:04:25 +0200

    Base

---
+++ PoleTimeGuesser/PoleTimeGuesser/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace PoleTimeGuesser;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ PoleTimeGuesser/PoleTimeGuesser/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace PoleTimeGuesser;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
