6126c732db03cdcf1e567a50879df20003fcabb1
Sthofy <suhajdakristof@gmail.com>
2022-10-15 18:16:35 +0200

    Adding DataGetter Service
    Using Ergast API.
    Implementing one endpoint for testing.

---
+++ PoleTimeGuesser/PoleTimeGuesser/Services/F1DataGetterService.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using PoleTimeGuesser.Model;
+
+namespace PoleTimeGuesser.Services
+{
+    public class F1DataGetterService : IF1DataGetterService
+    {
+        private readonly string _year = DateTime.Now.Year.ToString();
+        readonly HttpClient _httpClient;
+        List<DriverStandingsModel> driverStadingModel = new();
+
+        public F1DataGetterService()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        public async Task<List<DriverStandingsModel>> GetDriverStandings()
+        {
+            try
+            {
+                var response = await _httpClient.GetAsync("https://ergast.com/api/f1/current/driverStandings.json");
+                if (response.IsSuccessStatusCode)
+                {
+                    var result = await response.Content.ReadAsStringAsync();
+                    var json = JObject.Parse(result);
+                    var res = json["MRData"]["StandingsTable"]["StandingsLists"].First["DriverStandings"].ToString();
+                    driverStadingModel = JsonConvert.DeserializeObject<List<DriverStandingsModel>>(res);
+
+                    return driverStadingModel;
+                }
+                else
+                    return null;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+                return null;
+            }
+        }
+    }
+}
---
+++ PoleTimeGuesser/PoleTimeGuesser/Services/IF1DataGetterService.cs
@@ -0,0 +1,9 @@
+﻿using PoleTimeGuesser.Model;
+
+namespace PoleTimeGuesser.Services
+{
+    public interface IF1DataGetterService
+    {
+        Task<List<DriverStandingsModel>> GetDriverStandings();
+    }
+}
