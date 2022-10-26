023c873db5a74d2077fa0214a14b2920a40ceb89
Sthofy <suhajdakristof@gmail.com>
2022-10-26 19:19:48 +0200

    Add New API
    Replace the old api with a new one.

---
+++ PoleTimeGuesser/F1GuessAPI/GlobalUsings.cs
@@ -0,0 +1,6 @@
+﻿global using Microsoft.AspNetCore.Mvc;
+global using System.Diagnostics;
+global using HtmlAgilityPack;
+global using F1GuessAPI.Helpers;
+global using F1GuessAPI.Models;
+global using Newtonsoft.Json;
---
+++ PoleTimeGuesser/F1GuessAPI/Helpers/UrlHelper.cs
@@ -0,0 +1,23 @@
+﻿namespace F1GuessAPI.Helpers
+{
+    public class UrlHelper
+    {
+        HttpClient _httpClient = new HttpClient();
+        const string F1_SITE_URL = "https://www.formula1.com/en";
+
+        public async Task<string> CallUrl(string urlEnd)
+        {
+            try
+            {
+                string url = F1_SITE_URL + $"{urlEnd}";
+                var resopse = await _httpClient.GetStringAsync(url);
+                return resopse;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+                return null;
+            }
+        }
+    }
+}
