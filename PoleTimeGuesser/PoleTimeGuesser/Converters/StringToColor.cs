9c3ccb7a3aedc8e2dbe2db0dbf0f78a157650174
Sthofy <suhajdakristof@gmail.com>
2022-11-12 19:01:33 +0100

    Add Password requirements

---
+++ PoleTimeGuesser/PoleTimeGuesser/Converters/StringToColor.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Maui.Graphics.Converters;
+using System.Globalization;
+
+namespace PoleTimeGuesser.Converters
+{
+    internal class StringToColor : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            ColorTypeConverter converter = new ColorTypeConverter();
+
+            Color color = (Color)(converter.ConvertFromInvariantString((string)value));
+
+            return color;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string colorString = "Black";
+
+            return colorString;
+        }
+    }
+}
--- PoleTimeGuesser/PoleTimeGuesser/ViewModel/RegisterViewModel.cs
+++ PoleTimeGuesser/PoleTimeGuesser/ViewModel/RegisterViewModel.cs
@@ -14,7 +14,16 @@ namespace PoleTimeGuesser.ViewModel
         string _confirmPassword;
         [ObservableProperty]
         bool isProcessing;
+        [ObservableProperty]
+        string _lenTextColor = "red";
+        [ObservableProperty]
+        string _lTextColor = "red";
+        [ObservableProperty]
+        string _uTextColor = "red";
+        [ObservableProperty]
+        string _nTextColor = "red";
 
+        private bool _isStrongPassword = false;
         IServiceManager _serviceManager;
 
         public RegisterViewModel(IServiceManager serviceManager)
@@ -27,6 +36,7 @@ namespace PoleTimeGuesser.ViewModel
         async Task Register()
         {
             //TODO: Megvizsgálni hogy létezik e már a felhasználó
+            //TODO: Létrehozáskor beszúrni a scoreboard táblába 0 ponttal
 
             if (IsProcessing) return;
 
@@ -41,17 +51,18 @@ namespace PoleTimeGuesser.ViewModel
                 if (!Password.Equals(ConfirmPassword))
                     throw new Exception("Password and Confirm Password is not equal");
 
-                var request = new RegistrationRequest
-                {
-                    Username = Username,
-                    Password = Password,
-                    Email = Email,
-                };
 
-                var response = await _serviceManager.Registration(request);
-
-                if (response.StatusCode == 200)
+                if (_isStrongPassword)
                 {
+                    var request = new RegistrationRequest
+                    {
+                        Username = Username,
+                        Password = Password,
+                        Email = Email,
+                    };
+
+                    await _serviceManager.Registration(request);
+
                     await Shell.Current.GoToAsync($"{nameof(LoginView)}", true,
                         new Dictionary<string, object>
                         {
@@ -59,8 +70,6 @@ namespace PoleTimeGuesser.ViewModel
                             { "Password" , Password },
                         });
                 }
-                else
-                    await AppShell.Current.DisplayAlert("F1Guess", response.StatusMessage, "OK");
             }
             catch (Exception ex)
             {
@@ -70,6 +79,66 @@ namespace PoleTimeGuesser.ViewModel
             {
                 IsProcessing = false;
             }
+        }
+
+        [RelayCommand]
+        public void VerifyPassword()
+        {
+            bool lenght = false, lower = false, upper = false, number = false;
+            if (Password.Length > 8)
+            {
+                LenTextColor = "Green";
+                lenght = true;
+            }
+            else
+            {
+                LenTextColor = "Red";
+                lenght = false;
+            }
+            if (Password.Any(char.IsLower))
+            {
+                LTextColor = "Green";
+                lower = true;
+            }
+            else
+            {
+                LTextColor = "Red";
+                lower = false;
+            }
+            if (Password.Any(char.IsUpper))
+            {
+                UTextColor = "Green";
+                upper = true;
+            }
+            else
+            {
+                UTextColor = "Red";
+                upper = false;
+            }
+            if (Password.Any(char.IsNumber))
+            {
+                NTextColor = "Green";
+                number = true;
+            }
+            else
+            {
+                NTextColor = "Red";
+                number = false;
+            }
+
+            if (lenght && lower && upper && number)
+                _isStrongPassword = true;
+        }
+
+        private bool VerifyUsername()
+        {
+            var request = new GetByUsernameRequest
+            {
+                Username = Username,
+            };
+            var response = _serviceManager.CallWebAPI<GetByUsernameRequest, GetByUsernameResponse>("/User/GetByUsername", HttpMethod.Get, request).Result;
+
+            return response.IsExist;
         }
     }
 }
