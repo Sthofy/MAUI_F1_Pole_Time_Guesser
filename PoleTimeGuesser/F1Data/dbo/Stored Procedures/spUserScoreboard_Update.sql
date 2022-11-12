bd7faed12b39cd73d13e90d9fa79d381849c261b
Sthofy <suhajdakristof@gmail.com>
2022-11-12 19:05:19 +0100

    Add Update Scoreboard SP

---
+++ PoleTimeGuesser/F1Data/dbo/Stored Procedures/spUserScoreboard_Update.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [dbo].[spUserScoreboard_Update]
+	@UserId INT,
+	@Score INT
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	UPDATE [dbo].[UsersScoreboard]
+	SET Score=@Score
+	WHERE UserId=@UserId;
+END
