4d92c6b457a89574758ba818b6c1e5da665f5cb2
Sthofy <suhajdakristof@gmail.com>
2022-11-27 17:46:46 +0100

    Add Library

---
+++ PoleTimeGuesser/PoleTimeGuesser.Library/Models/LoggedInUserModel.cs
@@ -0,0 +1,11 @@
+﻿namespace PoleTimeGuesser.Library.Models
+{
+    public class LoggedInUserModel
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Token { get; set; }
+        public string AvatarSourceName { get; set; }
+    }
+}
